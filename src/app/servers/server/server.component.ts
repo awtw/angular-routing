d049bbf68df9cd4e6a3246db9b3dcf18757104da
awtw <alec.wang.tpe@gmail.com>
2020-01-28 01:14:05 +0800

    using query parameters allowEdit

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -13,7 +13,7 @@ export class ServerComponent implements OnInit, OnDestroy {
 	server: { id: number; name: string; status: string };
 	paramsSubscription: Subscription;
 
-	constructor(private serversService: ServersService, private route: ActivatedRoute) {}
+	constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) {}
 
 	ngOnInit() {
 		const id = +this.route.snapshot.params['id'];
@@ -21,6 +21,9 @@ export class ServerComponent implements OnInit, OnDestroy {
 		this.paramsSubscription = this.route.params.subscribe((params: Params) => {
 			this.server = this.serversService.getServer(+params['id']);
 		});
+	}
+	onEdit() {
+		this.router.navigate([ 'edit' ], { relativeTo: this.route });
 	}
 	ngOnDestroy() {
 		this.paramsSubscription.unsubscribe();
