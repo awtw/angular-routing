b0715450af8dc86d51217785dda7fb7d11bf7cc1
awtw <alec.wang.tpe@gmail.com>
2020-01-28 13:38:04 +0800

    child Routes with canActivateChild

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from './auth-guard.service';
 
 const appRoutes: Routes = [
 	{ path: '', component: HomeComponent },
@@ -19,6 +20,8 @@ const appRoutes: Routes = [
 	{
 		path: 'servers',
 		component: ServersComponent,
+		// canActivate: [ AuthGuard ], for all component
+		canActivateChild: [ AuthGuard ],
 		children: [ { path: ':id', component: ServerComponent }, { path: ':id/edit', component: EditServerComponent } ]
 	},
 	{
